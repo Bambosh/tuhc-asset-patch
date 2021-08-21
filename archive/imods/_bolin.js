a172d7a1ee8b5c48062dabaa1b29c2e5dcf56467
Gio <sethg@ipi.org>
2021-08-21 01:04:05 -0500

    Bolin/hqaudio imod prototypes

---
+++ archive/imods/_bolin.js
@@ -0,0 +1,45 @@
+var bolin_pages = ["002238", "002544", "002551", "002722", "002730", "002733", "002880", "002926", "002970", "003620"]
+var bolhq_pages = ["002544", "002551", "002733", "002880", "002926", "002970"]
+
+var looping_flashes = ['00980_1',]
+
+module.exports = {
+    hidden: true,
+    
+    edit(archive) {
+        console.log("Setting Bolin audio")
+        bolin_pages.forEach(page_num => {
+            const page = archive.mspa.story[page_num]
+
+            const filename = page.media[0].split('/').pop()
+            const plainname = filename.split(".").slice(0, -1).join(".")
+            const base_url = page.media[0].split("/").slice(0, -1).join("/")
+
+            if (page_num == "002880") {
+                archive.audioData[page.media[0]] = [
+                    {
+                        href: `${base_url}/${plainname}_bolin_1.mp3`,
+                        loop: (looping_flashes.includes(`${plainname}_1`))
+                    },
+                ]
+            } else {
+                if (bolhq_pages.includes(page_num)) {
+                    // what is the difference???
+                    archive.audioData[page.media[0]] = [
+                        {
+                            href: `${base_url}/${plainname}_bolin.mp3`,
+                            loop: (looping_flashes.includes(plainname))
+                        },
+                    ]
+                } else {
+                    archive.audioData[page.media[0]] = [
+                        {
+                            href: `${base_url}/${plainname}_bolin.mp3`,
+                            loop: (looping_flashes.includes(plainname))
+                        },
+                    ]
+                }
+            }
+        })
+    }
+}
