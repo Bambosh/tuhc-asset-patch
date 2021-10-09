db8d7a1583583df0edc6dd71ac48d3504a125f8a
Gio <sethg@ipi.org>
2021-10-09 00:31:05 -0500

    PXS tag update, hqaudio fix for YOU THERE BOY

--- archive/data/patch2.py
+++ archive/data/patch2.py
@@ -1,6 +1,8 @@
 import json
 import yaml
 import re
+import shutil
+import os
 
 with open("comics.json.bak", "r", encoding="utf-8") as fp:
     comics = json.load(fp)
@@ -9,7 +11,19 @@ with open("paradox-space.yaml", "r", encoding="utf-8") as fp:
     pxsdata = yaml.load(fp)
 
 with open("contributors.yaml", "r", encoding="utf-8") as fp:
-    comics['pxs']['contributors'] = yaml.load(fp)
+    # PXS reverse-alphabetical sorting
+    comics['pxs']['contributors'] = dict(reversed(sorted(yaml.load(fp).items(),key=lambda x:x[0])))
+
+with open("pxs_news.yaml", "r", encoding="utf-8") as fp:
+    comics['pxs']['newsfeed'] = yaml.load(fp)
+
+for name, contributor in comics['pxs']['contributors'].items():
+    workdir = "L:/Archive/Homestuck/Full Collections/TUHC/Asset Pack V1 - The  Unofficial Homestuck Collection/Asset Pack - The  Unofficial Homestuck Collection/archive/comics/pxs/avatar"
+    if contributor.get('avatar'):
+        __, ext = os.path.splitext(contributor['avatar'])
+        new_avatar = f"{name}{ext}"
+        shutil.copy2(os.path.join(workdir, contributor['avatar']), os.path.join(workdir, new_avatar))
+        comics['pxs']['contributors'][name]['avatar'] = new_avatar
 
 for i, comicid in enumerate(comics['pxs']['list']):
     ikey = str(i + 1)
